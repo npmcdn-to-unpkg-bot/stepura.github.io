8fcc12074ebc4c1c8bdce98760d723c7b1bfd9a7
stepura <stepura.v.g@gmail.com>
2016-03-02 12:48:58 +0300

    maria

--- portfolio/maria/js/main.js
+++ portfolio/maria/js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     $('#pagesection').fullpage({
     	sectionSelector: '.pagesection__item',
     	scrollingSpeed: 1000,
-    	resize: true,
     	navigation: true,
     	navigationTooltips: ['qw', 'we']
     });
