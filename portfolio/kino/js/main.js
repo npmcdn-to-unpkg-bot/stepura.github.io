d591fbc094933f0eec161a28f96f57c69eb1bf52
stepura <stepura.v.g@gmail.com>
2016-03-02 14:21:05 +0300

    -

--- portfolio/kino/js/main.js
+++ portfolio/kino/js/main.js
@@ -1,33 +1,8 @@
 $(document).ready(function() {
-  $('.infobox-search-box__search,.footer-search__input').focus(function() {
-    $(this).data('placeholder', $(this).attr('placeholder'))
-    $(this).attr('placeholder', '');
-  });
-  $('.infobox-search-box__search,.footer-search__input').blur(function() {
-    $(this).attr('placeholder', $(this).data('placeholder'));
-  });
-});
-
-$('.image-slider').slick({
-  fade: true,
-  autoplay: true,
-  autoplaySpeed: 4000
-});
-
-$('.content-slider').slick({
-  fade: true,
-  adaptiveHeight: true
-});
-
-$(".main-sidebar-image-gallery__item").fancybox({
-  beforeShow: function() {
-    var alt = this.element.find('img').attr('alt');
-    this.inner.find('img').attr('alt', alt);
-    this.title = alt;
-  }
-});
-
-
-$(window).load(function() {
-  $(".vertical-scroll-block").mCustomScrollbar();
+    $('#pagesection').fullpage({
+    	sectionSelector: '.pagesection__item',
+    	scrollingSpeed: 1000,
+    	navigation: true,
+    	navigationTooltips: ['свадебное фото', 'портреты', 'love story']
+    });
 });
