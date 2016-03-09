cfbc932fba1117e57f3b1013be7967591c32e932
stepura <stepura.v.g@gmail.com>
2016-03-09 15:08:46 +0300

    -

--- portfolio/maria/js/main.js
+++ portfolio/maria/js/main.js
@@ -5,4 +5,30 @@ $(document).ready(function() {
     	navigation: true,
     	navigationTooltips: ['свадебное фото', 'портреты', 'love story']
     });
+  });
+
+$('.grid').masonry({
+  // set itemSelector so .grid-sizer is not used in layout
+  itemSelector: '.grid-item',
+  // use element for option
+  columnWidth: '.grid-item',
+  percentPosition: true,
+  gutter: 0
+})
+
+var amountScrolled = 200;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+$('a.back-to-top, a.simple-back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
 });
